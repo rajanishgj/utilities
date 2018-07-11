694df91979ec5437b10af6f5d7249087047133a5
Rajanish GJ <rajanishgj@google.com>
2018-07-10 21:17:07 -0700

    fix install script path

--- apigee-kube/install.sh
+++ apigee-kube/install.sh
@@ -1,6 +1,6 @@
-gcloud container clusters create apigee-docker-planet --machine-type n1-standard-2 --num-nodes 6
-gcloud container clusters get-credentials apigee-docker-planet
-wget https://raw.githubusercontent.com/rajanishgj/utilities/master/apigee-kube/kube-apigee-install-scripts.zip
-unzip kube-apigee-install-scripts.zip
-./kube-apigee-install-scripts/devutils/kube-clean-setup.sh
+gcloud container clusters create apigee-docker-planet --machine-type n1-standard-2 --num-nodes 6 || echo "create cluster failed"
+gcloud container clusters get-credentials apigee-docker-planet || exit 1
+wget https://raw.githubusercontent.com/rajanishgj/utilities/master/apigee-kube/kube-apigee-install-scripts.zip || exit 1
+unzip kube-apigee-install-scripts.zip || exit 1
+./devutils/kube-clean-setup.sh || exit 1
 
